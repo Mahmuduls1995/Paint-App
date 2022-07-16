cc91c792383df5a4f5f0d56837fcd61dc2bb4587
mahmudul1995 <mahmuduls451@gmail.com>
2022-07-16 16:24:13 +0600

    done

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { Route, Routes } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./Components/Header/Header";
+import Login from "./Components/UserLogin/Login";
+import SignUp from "./Components/UserLogin/SignUp";
+import PrivateAuth from "./Components/UserLogin/PrivateAuth";
+import Canva from "./Components/Canva/Canva";
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={
+          <PrivateAuth>
+            <Canva></Canva>
+            {/* <Home></Home> */}
+          </PrivateAuth>
+        }></Route>
+        <Route path="/home" element={
+          <PrivateAuth>
+             <Canva></Canva>
+            {/* <Home></Home> */}
+          </PrivateAuth>
+        }></Route>
+        <Route path="/login" element={<Login></Login>} />
+        <Route path="/signup" element={<SignUp></SignUp>} />
+        <Route path="/canva" element={<Canva></Canva>} />
+
+      </Routes>
+    </div>
+  );
+}
+
+export default App
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,78 @@
+import { signOut } from 'firebase/auth';
+import React from 'react';
+import { Container, Nav,Navbar } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    const [user] = useAuthState(auth)
+
+    const handleSignOut = () => {
+        signOut(auth)
+    }
+    return (
+
+         <>
+
+            <Navbar collapseOnSelect className=" bg-emerald-400" sticky="top" expand="lg">
+                <Container>
+                    <Navbar.Brand as={Link} to="/">
+                       Paint App
+                    </Navbar.Brand>
+
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="m-auto">
+                            <Nav.Link className="text-white hover:bg-purple-700 ease-in duration-300  rounded" as={Link} to="/home">Home</Nav.Link>
+                            
+                            {/* <Nav>
+
+                                {
+                                    user ?
+                                        <div className="d-flex">
+                                            <Nav.Link className="text-white hover:bg-purple-700 ease-in duration-300 rounded mx-2" as={Link} to="/products">Products</Nav.Link>
+                                            <Nav.Link className="text-white hover:bg-purple-700 ease-in duration-300 rounded mx-2" as={Link} to="/AllPd">All Products</Nav.Link>
+
+                                            <Nav.Link className="text-white hover:bg-purple-700 ease-in duration-300 rounded mx-2" as={Link} to="/uploadPd">Upload Products</Nav.Link>
+
+                                            <Nav.Link className="text-white hover:bg-purple-700 ease-in duration-300 rounded mx-2" as={Link} to="/orders">Total Order</Nav.Link>
+
+                                        </div>
+                                        :
+                                        <Nav.Link className="text-white hover:bg-purple-700 ease-in duration-300 rounded mx-2"as={Link} to="/products" >Products</Nav.Link>
+
+                                }
+                            </Nav> */}
+
+
+                        </Nav>
+                        <Nav>
+
+                            {
+                                user ?
+                                    <span>
+                                        <button className="btn btn-primary mr-2" >{user.displayName}</button>
+                                        <button className="btn btn-primary" onClick={handleSignOut}>Sign Out</button>
+                                    </span>
+                                    :
+                                    <Nav.Link className="text-white hover:bg-purple-700 ease-in duration-300 rounded" eventKey={2} as={Link} to="login">
+                                        Login
+                                    </Nav.Link>
+
+                            }
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+
+        </>
+
+
+
+    );
+};
+
+export default Header;
